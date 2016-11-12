92589a35dcb3e6cb2df97b8dce16f9199690e33b
Lejla Zečević <lzecevic1@etf.unsa.ba>
2016-11-12 21:42:04 +0100

    Zavrsene validacije

--- script/skripta.js
+++ script/skripta.js
@@ -1,32 +1,41 @@
 // Funkcije za validacije formi - login, kontakt i sign up
 
+function showMessage(message, p) // Y U NO WORK??
+{
+    p.style.paddingTop = p.style.paddingBottom = "1.5%";
+    p.style.marginLeft = "-50px";
+    p.style.color = "white";
+    p.innerHTML(message);
+}
+
 function validacijaLogin()
 {
+    var p = document.getElementById("warningMessage");
     var email = document.getElementById("emailLogin").value;
     var password = document.getElementById("passwordLogin").value;
     if(!email && !password)
     {
-       var p = document.getElementById("warningMessage");
-       p.style.paddingTop = p.style.paddingBottom = "1.5%";
-       p.style.marginLeft = "-50px";
-       p.innerHTML = "Your email or password were incorrect."
-       return false;
+        p.style.paddingTop = p.style.paddingBottom = "1.5%";
+        p.style.marginLeft = "-50px";
+        p.style.color = "white";
+        p.innerHTML = "Your email or password were incorrect.";
+        return false;
     }
     if(!email)
     {
-       var p = document.getElementById("warningMessage");
-       p.style.paddingTop = p.style.paddingBottom = "1.5%";
-       p.style.marginLeft = "-50px";
-       p.innerHTML = "That's not a valid email address. Please try again."
-       return false;
+        p.style.paddingTop = p.style.paddingBottom = "1.5%";
+        p.style.marginLeft = "-50px";
+        p.style.color = "white";
+        p.innerHTML = "That's not a valid email address. Please try again.";
+        return false;
     }
     if(!password)
     {
-       var p = document.getElementById("warningMessage");
-       p.style.paddingTop = p.style.paddingBottom = "1.5%";
-       p.style.marginLeft = "-50px";
-       p.innerHTML = "You forgot to enter your password!";
-       return false;
+        p.style.paddingTop = p.style.paddingBottom = "1.5%";
+        p.style.marginLeft = "-50px";
+        p.style.color = "white";
+        p.innerHTML = "You forgot to enter your password!";
+        return false;
     }
 
     return true;
@@ -37,31 +46,86 @@ function validacijaKontakt()
     var name = document.getElementById("nameContact").value;
     var email = document.getElementById("emailContact").value;
     var message = document.getElementById("message").value;
+    var p = document.getElementById("warningMessage");    
 
     if(!name)
     {
-       var p = document.getElementById("warningMessage");
-       p.style.paddingTop = p.style.paddingBottom = "1.5%";
-       p.style.marginLeft = "-50px";
-       p.innerHTML = "You forgot to enter your name!";
-       return false;
+        p.style.paddingTop = p.style.paddingBottom = "1.5%";
+        p.style.marginLeft = "-50px";
+        p.style.color = "white";
+        p.innerHTML = "You forgot to enter your name!";
+        return false;
     }
-
     if(!email)
     {
-       var p = document.getElementById("warningMessage");
-       p.style.paddingTop = p.style.paddingBottom = "1.5%";
-       p.style.marginLeft = "-50px";
-       p.innerHTML = "That's not a valid email address. Please try again."
-       return false;
+        p.style.paddingTop = p.style.paddingBottom = "1.5%";
+        p.style.marginLeft = "-50px";
+        p.style.color = "white";
+        p.innerHTML = "That's not a valid email address. Please try again.";
+        return false;
     }
     if(!message)
     {
-       var p = document.getElementById("warningMessage");
-       p.style.paddingTop = p.style.paddingBottom = "1.5%";
-       p.style.marginLeft = "-50px";
-       p.innerHTML = "Please enter your message.";
-       return false;     
+        p.style.paddingTop = p.style.paddingBottom = "1.5%";
+        p.style.marginLeft = "-50px";
+        p.style.color = "white";
+        p.innerHTML = "Please enter your message.";
+        return false;     
     }
+
     return true;
 }
+
+function validacijaRegistracija()
+{
+    var name = document.getElementById("nameRegister").value; 
+    var surname = document.getElementById("surname").value;
+    var email = document.getElementById("emailRegister").value;
+    var password = document.getElementById("password1").value;
+    var passwordRepeat = document.getElementById("password2").value;
+    var p = document.getElementById("warningMessage");    
+    
+    if(!name)
+    {
+        p.style.paddingTop = p.style.paddingBottom = "1.5%";
+        p.style.marginLeft = "-50px";
+        p.style.color = "white";
+        p.innerHTML = "You forgot to enter your name!";
+        return false;
+    }
+    if(!surname)
+    {
+        p.style.paddingTop = p.style.paddingBottom = "1.5%";
+        p.style.marginLeft = "-50px";
+        p.style.color = "white";
+        p.innerHTML = "You forgot to enter your last name!";
+        return false;
+    }
+    if(!email)
+    {
+        p.style.paddingTop = p.style.paddingBottom = "1.5%";
+        p.style.marginLeft = "-50px";
+        p.style.color = "white";
+        p.innerHTML = "That's not a valid email address. Please try again.";
+        return false;
+    }
+    if(!password || !password)
+    {
+        p.style.paddingTop = p.style.paddingBottom = "1.5%";
+        p.style.marginLeft = "-50px";
+        p.style.color = "white";
+        p.innerHTML = "You forgot to enter your password!";
+        return false;
+    }
+    if(password !== passwordRepeat)
+    {
+        p.style.paddingTop = p.style.paddingBottom = "1.5%";
+        p.style.marginLeft = "-50px";
+        p.style.color = "white";
+        p.innerHTML = "Passwords don't match!";
+        return false;
+    }
+
+    return true;
+}
+
