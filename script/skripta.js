52a763483527ab4969ed6a2ba0427091f196e4ed
Lejla Zečević <lzecevic1@etf.unsa.ba>
2016-11-12 19:37:51 +0100

    Validacija kontakt forme

--- script/skripta.js
+++ script/skripta.js
@@ -1,3 +1,5 @@
+// Funkcije za validacije formi - login, kontakt i sign up
+
 function validacijaLogin()
 {
     var email = document.getElementById("emailLogin").value;
@@ -27,5 +29,39 @@ function validacijaLogin()
        return false;
     }
 
+    return true;
+}
+
+function validacijaKontakt()
+{
+    var name = document.getElementById("nameContact").value;
+    var email = document.getElementById("emailContact").value;
+    var message = document.getElementById("message").value;
+
+    if(!name)
+    {
+       var p = document.getElementById("warningMessage");
+       p.style.paddingTop = p.style.paddingBottom = "1.5%";
+       p.style.marginLeft = "-50px";
+       p.innerHTML = "You forgot to enter your name!";
+       return false;
+    }
+
+    if(!email)
+    {
+       var p = document.getElementById("warningMessage");
+       p.style.paddingTop = p.style.paddingBottom = "1.5%";
+       p.style.marginLeft = "-50px";
+       p.innerHTML = "That's not a valid email address. Please try again."
+       return false;
+    }
+    if(!message)
+    {
+       var p = document.getElementById("warningMessage");
+       p.style.paddingTop = p.style.paddingBottom = "1.5%";
+       p.style.marginLeft = "-50px";
+       p.innerHTML = "Please enter your message.";
+       return false;     
+    }
     return true;
 }
